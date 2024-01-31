7c4aaffd0251904abf6681a5215866817a104c05
rimadjamaa <rimadjamaa7@gmail.com>
2024-01-31 01:42:34 -0800

    use pointer str[n:n]

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[str.find("object-oriented"):str.find("object-oriented") + 28] + str[str.find("with"):str.find("with")+5] + str[:6])
+print(str[39:67] + str[107:112] + str[:6])
 
