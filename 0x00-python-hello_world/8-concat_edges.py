ceb46bdf54f77899d7c3f5fb717abc1502a7d57f
rimadjamaa <rimadjamaa7@gmail.com>
2024-01-31 01:34:38 -0800

    use pointer str.find()

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find("object-oriented"):str.find("object-oriented") + 28] + str[str.find("with"):str.find("with")+5] + str[:6]
-print(str)
+print(str[str.find("object-oriented"):str.find("object-oriented") + 28] + str[str.find("with"):str.find("with")+5] + str[:6])
+
