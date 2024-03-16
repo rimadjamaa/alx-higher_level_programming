cc84bdb99337836ab6ea02a9e61801df4d058da5
rimadjamaa <rimadjamaa7@gmail.com>
2024-03-16 03:24:55 -0700

    task 0

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,8 +3,8 @@
 def new_in_list(my_list, idx, element):
     # Check if idx is negative or out of range
     if idx < 0 or idx >= len(my_list):
-        return my_list[:]  # Return a copy of the original list
-    
+        return my_list[:]
+    # Return a copy of the original list
     # Create a new list with the modified element
     new_list = my_list[:]
     new_list[idx] = element
