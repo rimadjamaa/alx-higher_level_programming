3e2d42f1e3d48a8bdbb8bda2ef48b9ed2b668d2d
rimadjamaa <djamaarima7@gmail.com>
2024-07-30 16:03:27 -0700

    update 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Get the byte size of the HTTP response header for a given URL.
-curl -s "$1" | wc -c
+url="$1"
+
+# Use head command to get only the header and pipe it to wc -c
+curl -s "$url" | wc -c
